e8e4307f3f69d71b41572a189826217d4e6db540
jupanaos <julie.pariona@gmail.com>
2023-02-24 02:27:34 +0100

    feat: add todo service

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/Todo';
-import { TODOS } from 'src/app/mock-todos';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
-  todos: Todo[] = TODOS;
+export class TodosComponent implements OnInit{
+  todos: Todo[] = [];
+
+  constructor(private todoService: TodoService){}
+
+  ngOnInit(): void {
+    this.todoService.getTodos().subscribe((todos) => (this.todos = todos));
+  }
 }
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Todo } from '../Todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  private apiUrl = 'http://localhost:5000/todos';
+
+  constructor(private http: HttpClient) { }
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiUrl);
+  }
+}
