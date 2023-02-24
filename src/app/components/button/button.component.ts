bf6f5ad481dda0f8234ee2e8adf84fcfc346513d
jupanaos <julie.pariona@gmail.com>
2023-02-24 01:35:26 +0100

    feat: base components

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Input() text!: string;
+  @Output() btnClick = new EventEmitter();
+
+  handleClick() {
+    this.btnClick.emit();
+  }
+}
